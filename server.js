8af0e71c049cd55789cf24db39204e42c852c13b
Zach Retterath <ztretterath@gmail.com>
2016-11-01 22:17:49 -0400

    Commit before installing dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+//Express
+var express = require('express');
+var app = express();
+
+var path = require('path');
+
+
+// Models
+
+//Debugging
+var logger = require('morgan');
+app.use(logger('dev'));
+
+//Static Assets
+app.use(express.static(__dirname + '/public'));
+
+//Config heroku
+// var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/wanderluster';
+// mongoose.connect(mongoURI);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("=============================");
+  console.log("SERVER LISTENING ON PORT 3000");
+  console.log("=============================");
+});
